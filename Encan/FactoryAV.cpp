c229f74f87636a25bb532a7ce6f34318471c78d4
RemiF <superteam1nsa@robot2.me>
2018-11-09 09:35:48 -0500

    Refactoring createAcheteut Le template perd en intérêt mais pour laisser la code réutilisable il a été laissé

--- Encan/FactoryAV.cpp
+++ Encan/FactoryAV.cpp
@@ -10,36 +10,18 @@ enum Description
 
 
 auto FactoryAV::createAcheteurs() {
+	Acheteurs<Interet>* type[5] = {
+	new Acheteurs<Interet>(500, EtatAcheteur::AGRESSIF, Interet(Description::ART, 200)),
+	new Acheteurs<Interet>(1000, EtatAcheteur::PATIENT),
+	new Acheteurs<Interet>(1500, EtatAcheteur::LENT, Interet(Description::SERVICE, 100)),
+	new Acheteurs<Interet>(100, EtatAcheteur::RAPIDE, Interet(Description::ART, 50)) ,
+	new Acheteurs<Interet>(250, EtatAcheteur::MODERE, Interet(Description::SERVICE, 300)) };
+	return type[static_cast<int>(round(4 * rand() / RAND_MAX))];//(b-a)*[0-1] +a
 
-	int alea = round((4 * rand() / RAND_MAX) + 1);
-	alea = 1;
-	switch (alea)
-	{
-	case 1:
-		return new Acheteurs<Interet>(500, EtatAcheteur::AGRESSIF, Interet(Description::ART, 200));
 		//return new Acheteurs(500, new Interet(Description::ART, 300), EtatAcheteur::AGRESSIF);
 		//return new Acheteurs(500, std::ref(*(new Interet(Description::ART, 300))), EtatAcheteur::AGRESSIF);
 		//return new Acheteurs(500, (new Interet(Description::ART, 300))->operator(), EtatAcheteur::AGRESSIF);
 		//return new Acheteurs<Art>(500, TypeAcheteur::AGRESSIF);
-		break;
-	case 2:
-		//intérêt par défaut #foncteur pur
-		return new Acheteurs<Interet>(1000, EtatAcheteur::PATIENT);
-		//return new Acheteurs<Interet>(1000, Interet(Description::ANTIQUITE, 300), EtatAcheteur::PATIENT);
-		break;
-	case 3:
-		return new Acheteurs<Interet>(1500, EtatAcheteur::LENT, Interet(Description::SERVICE, 100));
-		break;
-	case 4:
-		return new Acheteurs<Interet>(100, EtatAcheteur::RAPIDE, Interet(Description::ART, 50));
-		break;
-	case 5:
-		return new Acheteurs<Interet>(250, EtatAcheteur::MODERE, Interet(Description::SERVICE, 300));
-		break;
-	default:
-		printf("Erreur dans la génération du nombre aléatoire");
-		break;
-	}
 }
 template<class T>
 Vendeurs<T>* FactoryAV::createVendeurs()
@@ -51,10 +33,10 @@ Vendeurs<T>* FactoryAV::createVendeurs()
 		return new Vendeurs<Art>(FactoryObjetEncan::createArt());
 		break;
 	case 2:
-		return new Vendeurs<Antiquite>(FactoryObjetEncan::createService());
+		return new Vendeurs<Antiquite>(FactoryObjetEncan::createAntiquite());
 		break;
 	case 3:
-		return new Vendeurs<Antiquite>(FactoryObjetEncan::createService());
+		return new Vendeurs<Service>(FactoryObjetEncan::createService());
 		break;
 	}
 }
