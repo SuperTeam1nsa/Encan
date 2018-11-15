2fddd5fb0772377e644867c81788630c2368b3ef
peejeeman <benard@etud.insa-toulouse.fr>
2018-11-15 14:04:55 -0500

    Ajout des classes Art et Services

--- Encan/Art.h
+++ Encan/Art.h
@@ -1,7 +1,43 @@
 ﻿#pragma once
+#include <string>
+
 class Art
 {
 public:
-	Art() {};
+	enum Type
+	{
+		LIVRE, PEINTURE, SCULPTURE, INCONNU
+	};
 	~Art() {};
+
+	int getRenomee() const { return renomee; }
+	int getValeur() const { return valeur; }
+	int getVolume() const { return volume; }
+	Type getType() const { return type; }
+	std::string getDescription() const { return description; }
+
+private:
+	Art(int renomee, int valeur, int volume, Type type, std::string description)
+	{
+		setRenommee(renomee);
+		setValeur(valeur);
+		setVolume(volume);
+		setType(type);
+		setDrescription(description);
+	};
+
+	Art(Type type) : Art(1, 100, 10, type, "Inconnu") {}
+	Art() : Art(1, 100, 10, INCONNU, "Inconnu") {}
+
+	void setRenommee(int renomee) { this->renomee = renomee; }
+	void setValeur(int valeur) { this->valeur = valeur; }
+	void setVolume(int volume) { this->volume = volume; }
+	void setType(Type type) { this->type = type; }
+	void setDrescription(std::string descriptiion) { this->description = descriptiion; }
+
+	int renomee;
+	int valeur;
+	int volume;
+	Type type;
+	std::string description;
 };
--- Encan/Service.h
+++ Encan/Service.h
@@ -2,6 +2,36 @@
 class Service
 {
 public:
-	Service() {};
-	~Service() {};
+	~Service() {}
+	enum Type
+	{
+		LIVRAISON, VOYAGE, MENAGE, CUISINE, COURS, INCONNU
+	};
+
+	int getFrais() const { return frais; }
+	int getTarif() const { return tarif; }
+	Type getType() const { return type; }
+	int getXp() const { return xp; }
+
+private:
+	Service(int frais, int tarif, Type type, int xp)
+	{
+		setFrais(frais);
+		setTarif(tarif);
+		setType(type);
+		setXp(xp);
+	}
+	Service(Type type) : Service(10, 100, type, 50) {}
+	Service() : Service(10, 100, INCONNU, 50) {}
+
+	int frais;
+	int tarif;
+	Type type;
+	int xp;
+
+
+	void setFrais(int frais) { this->frais = frais; }
+	void setTarif(int tarif) { this->tarif = tarif; }
+	void setType(Type type) { this->type = type; }
+	void setXp(int xp) { this->xp = xp; }
 };
