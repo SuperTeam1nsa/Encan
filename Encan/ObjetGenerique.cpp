6417a93281f196a5a54eb066e556a5a8dc82647a
peejeeman <benard@etud.insa-toulouse.fr>
2018-11-15 16:54:33 -0500

    Ajout constructeurs en surcharge dans ObjetGenerique pour creer un struct Carac globale

--- Encan/ObjetGenerique.cpp
+++ Encan/ObjetGenerique.cpp
@@ -1,11 +1,26 @@
 #include "pch.h"
 #include "ObjetGenerique.h"
 
-
-ObjetGenerique::ObjetGenerique()
+ObjetGenerique::ObjetGenerique(CaracAnt myCarac)
 {
+	this->myCarac = { myCarac.description, myCarac.etat, myCarac.periode, myCarac.valeur,
+	-1, -1, -1, TypeArt::INCONNU, "",
+	-1, -1, TypeSer::INCONNU, -1 };
 }
 
+ObjetGenerique::ObjetGenerique(CaracArt myCarac)
+{
+	this->myCarac = { "", Etat::NEUF, Periode::INCONNUE, -1,
+	myCarac.renomee, myCarac.valeur, myCarac.volume, myCarac.type, myCarac.description,
+	-1, -1, TypeSer::INCONNU, -1 };
+}
+
+ObjetGenerique::ObjetGenerique(CaracSer myCarac)
+{
+	this->myCarac = { "", Etat::NEUF, Periode::INCONNUE, -1,
+	-1, -1, -1, TypeArt::INCONNU, "",
+	myCarac.frais, myCarac.tarif, myCarac.type, myCarac.xp };
+}
 
 ObjetGenerique::~ObjetGenerique()
 {
